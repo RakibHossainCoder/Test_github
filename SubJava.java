082b903741c1257b7bd2d55e6b982d9496ace7f9
Md Rakib Hossain <ghrakib1382@gmail.com>
2024-11-18 03:52:30 -0600

    text file added by me

---
+++ SubJava.java
@@ -0,0 +1,16 @@
+
+public class SubJava {
+    
+    
+    public static void main(String[] args) {
+        
+        int a= 400;
+        int b= 10;
+        
+        int div =a/b ;
+        System.out.print(div);
+        
+              
+        
+    } 
+}
